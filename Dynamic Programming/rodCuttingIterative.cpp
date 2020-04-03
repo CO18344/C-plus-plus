1b335e6bd328f698c2db58f442eceb8eb8163b77
CO18344 <42909454+CO18344@users.noreply.github.com>
2020-04-03 17:23:47 +0530

    Update rodCuttingIterative.cpp

--- Dynamic Programming/rodCuttingIterative.cpp
+++ Dynamic Programming/rodCuttingIterative.cpp
@@ -1,39 +1,38 @@
 #include<iostream>
 #include<limits.h>
 using namespace std;
-int RodCutting(int *price,int n,int *r)
+
+void rodCutting(int *price,int n,int *r)
 {
-	int q=INT_MIN;
-	if(r[n-1]>=0)
-		return r[n-1];
-	if(n-1==0)
-		return 0;
-	else
+	int q;
+	r[0]=0;
+	for(int i=1;i<=n;i++)
 	{
-		
-		for(int i=0;i<n;i++)
+		q=INT_MIN;
+		for(int j=1;j<=i;j++)
 		{
-			q=max(q,price[i]+RodCutting(price,n-1-i,r));
+			q=max(q,price[j-1]+r[i-j]);
 		}
-		
+		r[i]=q;
+		cout<<r[i]<<endl;
 	}
-	r[n-1]=q;
-	return q;
+	
+
 }
 int main()
 {
+	//cout<<"Let's duel";
+	
 	int price[]={1,5,8,9,10,17,17,20,24,30};
 	int length=sizeof(price)/sizeof(int);
 	int *r= new int[length];
-	for(int i=0;i<length;i++)
+	for(int i=0;i<=length;i++)
 	{
 		*(r+i)=INT_MIN;
 
-		cout<<r[i]<<endl;
+		//cout<<r[i]<<endl;
 	}
 	int n=10;
-	
-	cout<<RodCutting(price,n,r)<<endl;
-	
+	rodCutting(price,n,r);
 	return 0;
 }
